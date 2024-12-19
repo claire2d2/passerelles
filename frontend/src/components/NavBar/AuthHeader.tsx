d5d2c8b8eb050be30ce35f1abd193589cbf1747d
Claire <claireyuan.song@gmail.com>
2024-12-19 22:55:43 +0100

    fix for first deploy

--- frontend/src/components/NavBar/AuthHeader.tsx
+++ frontend/src/components/NavBar/AuthHeader.tsx
@@ -1,26 +1,23 @@
-import { useState } from "react"
-import { HiArrowRightStartOnRectangle } from "react-icons/hi2";
+// import { useState } from "react"
+// import { HiArrowRightStartOnRectangle } from "react-icons/hi2";
 
 const AuthHeader = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+	const AuthStyle = "flex flex-row gap-2 w-full justify-center font-medium";
+	const buttonStyle =
+		"border-solid border-2 border-md border-white rounded-md p-2 text-sm hover:bg-white hover:text-text active:bg-maindarker active:text-white";
+	return (
+		<>
+			<div className={AuthStyle}>
+				<div className={buttonStyle}>Se connecter</div>
+				<div className={buttonStyle}>S'enregistrer</div>
+			</div>
 
-  const AuthStyle = "flex flex-row gap-2 w-full justify-center font-medium"
-  const buttonStyle = "border-solid border-2 border-md border-white rounded-md p-2 text-sm hover:bg-white hover:text-text active:bg-maindarker active:text-white"
-    return (
-        <>
-  {!isLoggedIn && <div className={AuthStyle}>
-      <div className={buttonStyle}>Se connecter</div>
-      <div className={buttonStyle}>S'enregistrer</div>
-    </div>}
-
-    {isLoggedIn && <div className={AuthStyle}>
+			{/* {isLoggedIn && <div className={AuthStyle}>
         <span>Bonjour, Claire !</span>
         <HiArrowRightStartOnRectangle/>
-        </div> }
-    
-    
-    </>
-  )
-}
+        </div> } */}
+		</>
+	);
+};
 
-export default AuthHeader
+export default AuthHeader;
