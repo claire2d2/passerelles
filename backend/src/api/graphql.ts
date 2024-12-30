694c6500e10b1b1c668fc3fd68aee60f0a12bab9
Claire <claireyuan.song@gmail.com>
2024-12-30 12:12:10 +0100

    fix for deploy

--- backend/src/api/graphql.ts
+++ backend/src/api/graphql.ts
@@ -1,4 +1,5 @@
 // api/graphql.js
-const { handler } = require('../index'); // Import the handler from the correct file
+const { handler } = require('../dist/index'); // Import the handler from the correct file
+console.log("Handler function is being called");  
 
 module.exports = handler; // Export the handler for Vercel
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,25 @@
 import "reflect-metadata";
 import "module-alias/register";
 
-import depthLimit from 'graphql-depth-limit';
-import { ApolloServer } from '@apollo/server';
-const { startServerAndCreateNextHandler } = require('@as-integrations/next');
-import typeDefs from './typeDefs';
-import resolvers from './resolvers';
-import { datasource } from './lib/datasource';
+import depthLimit from 'graphql-depth-limit'
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import typeDefs from "./typeDefs";
+import resolvers from "./resolvers";
+import {datasource} from "./lib/datasource";
 
-// Create Apollo Server instance
-export const server = new ApolloServer({
+
+export const server = new ApolloServer<{}>({
   typeDefs,
   resolvers,
-  validationRules: [depthLimit(5)], // Prevent deep queries (n+1 issue)
+  validationRules: [depthLimit(5)] //n+1
 });
-
-export const handler = startServerAndCreateNextHandler(server, { path: "/api/graphql" });
+async function main() {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+    
+  });
+  await datasource.initialize(); //initialisation de la base de données
+  console.log(`🚀  Server ready at: ${url}`);
+}
+main();
